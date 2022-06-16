b4efb53cdcb25d48d38050dced451afdd1515cc5
Haikkki <15801356532@163.com>
2022-06-16 10:04:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author haikkki
+ * @create 2022-06-16 9:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
